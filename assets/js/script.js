a8c3c01a16e9d80903eb29db592e883b6552e355
andreaslarssamils <12srnmai@students.codeinstitute.net>
2025-05-18 07:32:50 +0200

    Add initial HTML, CSS, and JavaScript files for Love Math project

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+console.log('Hello World');
+// This is a simple JavaScript file that logs "Hello World" to the console.
+// It can be used to test if the JavaScript file is correctly linked to the HTML file.
