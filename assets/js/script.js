b36003f122863ba891e4e74106e1902434913b0c
andreaslarssamils <larssamils.andreas@gmail.com>
2025-05-21 05:38:06 +0200

    Implement button click event handling for game interactions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+// Wait for the DOM to load before running the game
+document.addEventListener('DOMContentLoaded', function () {
+  let buttons = document.getElementsByTagName('button');
+
+  for (let button of buttons) {
+    button.addEventListener('click', function () {
+      if (this.getAttribute('data-type') === 'submit') {
+        alert('You clicked Submit!');
+      } else {
+        let gameType = this.getAttribute('data-type');
+        alert(`You clicked ${gameType}`);
+      }
+    });
+  }
+});
+
 function runGame() {}
 
 function checkAnswer() {}
